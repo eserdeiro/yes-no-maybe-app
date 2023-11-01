d2ca46f6d5230fac26dd3c1dca168652df71b8d6
eserdeiro <eserdeiro@gmail.com>
2023-11-01 19:13:35 -0300

    Dio installed, no implemented

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
 
   final ScrollController chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
 
 List<Message> messageList = [
   Message(text: 'Hi', fromWho: FromWho.me),
@@ -16,6 +18,9 @@ Future<void> sendMessage(String text) async{
   if(text.isEmpty) return;
  final newMessage = Message(text: text, fromWho: FromWho.me);
 messageList.add(newMessage);
+  if(text.endsWith('?')) {
+    herReply();
+  }
 notifyListeners();
 moveScrollToBottom();
 }
@@ -27,6 +32,10 @@ Future<void> receiveMessage(String text) async{
  messageList.add(newMessage);
 notifyListeners();
 moveScrollToBottom();
+}
+
+  Future<void>herReply() async {
+  final herMessage = await getYesNoAnswer.getAnswer();
 }
 
 //When user send message
--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -3,60 +3,59 @@ import 'package:yes_no_app/domain/entities/message.dart';
 
 class HerMessageBubble extends StatelessWidget {
   final Message message;
-  const HerMessageBubble ({super.key, required this.message});
+  const HerMessageBubble({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
-            decoration:  BoxDecoration(
-            color: colors.secondary,
-            borderRadius: BorderRadius.circular(20)
-            ),
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text(message.text, 
-                        style: const TextStyle(color: Colors.white)),
+            child:
+                Text(message.text, style: const TextStyle(color: Colors.white)),
           ),
         ),
         const SizedBox(height: 5),
         //Todo image
         _ImageBubble(),
         const SizedBox(height: 10),
-        ],
+      ],
     );
   }
 }
 
 class _ImageBubble extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final colors = Theme.of(context).colorScheme;
 
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.network('https://yesno.wtf/assets/yes/9-6403270cf95723ae4664274db51f1fd4.gif',
-      width: size.width * 0.7,
-      height: 150,
-      fit: BoxFit.cover,
-      loadingBuilder: (context, child, loadingProgress) {
-          if (loadingProgress == null)  return child;
-          return Container(
-            color: colors.secondary,
-            width: size.width * 0.7,
-            height: 150,
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child:   const Center(child: Text('Loading image...',)),
-          );
-      },
-      )
-      );
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+          'https://yesno.wtf/assets/yes/9-6403270cf95723ae4664274db51f1fd4.gif',
+          width: size.width * 0.7,
+          height: 150,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              color: colors.secondary,
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Center(
+                  child: Text(
+                'Loading image...',
+              )),
+            );
+          },
+        ));
   }
 }
